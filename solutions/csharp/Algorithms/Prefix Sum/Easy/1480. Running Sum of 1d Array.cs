50792228c1efbc2f47cd1f45d3ff51b7a81b9a18
efchbd1013 <efchbd1013@gmail.com>
2025-06-10 15:58:40 +0300

    Add 1480

---
+++ solutions/csharp/Algorithms/Prefix Sum/Easy/1480. Running Sum of 1d Array.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        int[] arr = new int[nums.Length];
+        int sum=0;
+        for(int i=0; i< nums.Length; i++)
+        {
+            sum+=nums[i];
+            arr[i]=sum;
+        }
+        return arr;
+    }
+}
